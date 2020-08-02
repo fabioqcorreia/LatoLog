ee77c9230fe4f3989d2306693a96e1a3f64bb918
fabioqcorreia <fabioqcorreia@gmail.com>
2020-08-02 19:26:38 -0300

    Colorizing build.sh and error improvements

--- build.sh
+++ build.sh
@@ -1,34 +1,58 @@
 #! /bin/bash
 
+Red='\e[38;2;241;23;18m'          # Red
+Green='\e[38;2;28;237;137m'        # Green
+Yellow='\e[38;2;255;229;82m'       # Yellow
+Teal='\e[38;2;76;220;220m'
+
+error_exit()
+{
+	echo -e "$1" 1>&2
+	exit 1
+}
+
 build () {
-  echo "Cleaning old builds..."
+  echo -e "$Green \n"
+  echo -e " _           _        _                  "
+  echo -e "| |         | |      | |                 "
+  echo -e "| |     __ _| |_ ___ | |     ___   __ _  "
+  echo -e "| |    / _\` | __/ _ \| |    / _ \ / _\` | "
+  echo -e "| |___| (_| | || (_) | |___| (_) | (_| | "
+  echo -e "\_____/\__,_|\__\___/\_____/\___/ \__, | "
+  echo -e "                                   __/ | "
+  echo -e "                                  |___/ "
+  
+  echo -e "Cleaning old builds...$Teal"
   rm -rf dist/ build/ LatoLog.egg-info/ || true
 
-  echo "Creating new builds"
-  python3 setup.py sdist bdist_wheel || echo "Error building files, check for more info on output"
+  echo -e "$Green"
+  echo -e "Creating new builds$Teal\n"
+  python3 setup.py sdist bdist_wheel || error_exit "$Red\nError building files, check for more info on output"
 
-  echo "Checking with twine..."
-  twine check dist/* || echo "Error while checking files with twine"
-
-  echo "Done!"
+  echo -e "$Green"
+  echo -e "Checking with twine...$Teal\n"
+  twine check dist/* || error_exit "$Red\nError while checking files with twine"
+  echo -e "$Green"
+  echo -e "Done!\n"
 }
 
 test_upload () {
   build
-  echo "Uploading to PyPi test repository..."
-  python3 -m twine upload --repository testpypi dist/*
-  echo "Done!"
+  echo -e "$Green\nUploading to PyPi test repository...$Teal"
+  python3 -m twine upload --repository testpypi dist/* || error_exit "$Red\nError uploading files, check for more info on output"
+  echo -e "$Green\nDone!\n"
 }
 
 prod_upload () {
   build
-  echo "Uploading to PyPi main repository..."
-  python3 -m twine upload dist/*
-  echo "Done!"
+  echo -e "$Green\nUploading to PyPi main repository...$Teal"
+  python3 -m twine upload dist/* || error_exit "$Red\nError uploading files, check for more info on output"
+  echo -e "$Green\nDone!\n"
 }
 
 help () {
-  echo -ne "You have three options: \n"\
+  echo -ne "$Yellow\n"\
+           "You have three options: \n"\
            " - build \n"\
            " - test_upload\n"\
            " - prod_upload\n"\
@@ -43,5 +67,3 @@ case $1 in
   "help") help;;
   *) echo -ne "Wrong argument or not supported.\n\n"; help;;
 esac
-
-
